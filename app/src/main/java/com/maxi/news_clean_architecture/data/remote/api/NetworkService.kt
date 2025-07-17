2166ad1003b24dfef3bb1fdc4ec5de1b42a88ab0
maxi <arora.anubhav93@gmail.com>
2025-07-17 23:34:40 +0530

    Add news interface method

---
+++ app/src/main/java/com/maxi/news_clean_architecture/data/remote/api/NetworkService.kt
@@ -0,0 +1,19 @@
+package com.maxi.news_clean_architecture.data.remote.api
+
+import com.maxi.news_clean_architecture.data.remote.ApiConstants.Endpoints.TOP_HEADLINES
+import com.maxi.news_clean_architecture.data.remote.ApiConstants.QueryParams.COUNTRY
+import com.maxi.news_clean_architecture.data.remote.ApiConstants.QueryParams.DEFAULT_COUNTRY
+import com.maxi.news_clean_architecture.data.remote.ApiConstants.QueryParams.DEFAULT_LANGUAGE
+import com.maxi.news_clean_architecture.data.remote.ApiConstants.QueryParams.LANGUAGE
+import com.maxi.news_clean_architecture.domain.model.NewsResponse
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface NetworkService {
+
+    @GET(TOP_HEADLINES)
+    suspend fun getNews(
+        @Query(LANGUAGE) language: String = DEFAULT_LANGUAGE,
+        @Query(COUNTRY) country: String = DEFAULT_COUNTRY
+    ): NewsResponse
+}
