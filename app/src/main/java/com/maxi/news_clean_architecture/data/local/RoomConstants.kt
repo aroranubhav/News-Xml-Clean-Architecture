6ca88889520e1a35a26842acab59fb728ddd687d
maxi <arora.anubhav93@gmail.com>
2025-07-18 13:31:56 +0530

    Add constants for room db setup

---
+++ app/src/main/java/com/maxi/news_clean_architecture/data/local/RoomConstants.kt
@@ -0,0 +1,23 @@
+package com.maxi.news_clean_architecture.data.local
+
+object RoomConstants {
+
+    object Database {
+
+        const val NEWS = "news"
+    }
+
+    object TABLE {
+
+        const val ARTICLES = "articles"
+    }
+
+    object COLUMNS {
+
+        const val AUTHOR = "author"
+        const val TITLE = "title"
+        const val DESCRIPTION = "description"
+        const val URL = "url"
+        const val IMAGE_URL = "image_url"
+    }
+}
