228def6738824159d7a5e0c0b7b6af695d8e68e0
maxi <arora.anubhav93@gmail.com>
2025-07-18 13:44:37 +0530

    Add DAO in the repository DI provider

--- app/src/main/java/com/maxi/news_clean_architecture/di/module/NewsRepositoryModule.kt
+++ app/src/main/java/com/maxi/news_clean_architecture/di/module/NewsRepositoryModule.kt
@@ -1,5 +1,6 @@
 package com.maxi.news_clean_architecture.di.module
 
+import com.maxi.news_clean_architecture.data.local.dao.NewsDao
 import com.maxi.news_clean_architecture.data.remote.api.NetworkService
 import com.maxi.news_clean_architecture.data.repository.DefaultNewsRepository
 import com.maxi.news_clean_architecture.domain.repository.NewsRepository
@@ -18,9 +19,11 @@ class NewsRepositoryModule {
     @Singleton
     fun provideNewsRepository(
         networkService: NetworkService,
+        newsDao: NewsDao,
         dispatcherProvider: DispatcherProvider
     ): NewsRepository = DefaultNewsRepository(
         networkService,
+        newsDao,
         dispatcherProvider
     )
 }
