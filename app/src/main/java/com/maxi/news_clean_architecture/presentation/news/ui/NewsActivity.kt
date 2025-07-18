3983d54909e3e7df87348871831525c97fac9a6f
maxi <arora.anubhav93@gmail.com>
2025-07-18 12:55:47 +0530

    Add viewbinding and init viewmodel

--- app/src/main/java/com/maxi/news_clean_architecture/presentation/news/ui/NewsActivity.kt
+++ app/src/main/java/com/maxi/news_clean_architecture/presentation/news/ui/NewsActivity.kt
@@ -3,12 +3,23 @@ package com.maxi.news_clean_architecture.presentation.news.ui
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import com.maxi.news_clean_architecture.R
+import androidx.lifecycle.ViewModelProvider
+import com.maxi.news_clean_architecture.databinding.ActivityNewsBinding
+import com.maxi.news_clean_architecture.presentation.news.viewmodel.NewsViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class NewsActivity : AppCompatActivity() {
+
+    private lateinit var viewModel: NewsViewModel
+
+    private lateinit var binding: ActivityNewsBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_news)
+        binding = ActivityNewsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        viewModel = ViewModelProvider(this)[NewsViewModel::class]
     }
 }
